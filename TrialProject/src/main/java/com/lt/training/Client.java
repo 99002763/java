45cf8919427dc0d373e90630769f915d1cb7748d
99002763 <72455156+99002763@users.noreply.github.com>
2020-10-20 16:16:04 +0530

    Annotations

---
+++ TrialProject/src/main/java/com/lt/training/Client.java
@@ -0,0 +1,16 @@
+package com.lt.training;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Client {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context = new AnnotationConfigApplicationContext("com");
+		GreetService greetService = context.getBean("greetServiceImpl",GreetService.class );
+		System.out.println(greetService.greet("ABC"));
+	}
+
+}
